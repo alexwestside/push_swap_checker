93f9cc568a0d3d864d98b10a0ac8c31c424f4d3e
orizhiy <sklif.85@gmail.com>
2017-02-26 20:45:14 +0200

    psch

---
+++ cmake-build-debug/CMakeFiles/push_swap_checker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/push_swap_checker.dir/main.cpp.o"
+  "push_swap_checker.pdb"
+  "push_swap_checker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/push_swap_checker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
