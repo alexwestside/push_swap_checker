ffc6bf1a4d8d0e030a29f798b5c753d3cbb0779e
orizhiy <sklif.85@gmail.com>
2017-03-09 22:24:25 +0200

    checker_7_up

--- move_stack.c
+++ move_stack.c
@@ -113,37 +113,3 @@ void ft_push(t_stack **a, t_stack **b, int id)
     }
     //ft_printf("%s\n", !id ? "pa" : "pb");
 }
-
-/*
-void ft_push_a(t_stack **a, t_stack **b)
-{
-    t_stack *list;
-    t_stack *tail;
-
-    list = *b;
-    tail = *a;
-    if (!*b)
-    {
-        //list = *a;
-        *b = *a;
-        list = list->next;
-        *a = list;
-        list->prev = NULL;
-        (*b)->next = NULL;
-        (*b)->prev = NULL;
-    }
-    else if (*a)
-    {
-        //list = *a;
-        //tail = *b;
-        tail->prev = *a;
-        tail = tail->prev;
-        list = list->next;
-        *a = list;
-        tail->next = *b;
-        *b = tail;
-        list->prev = NULL;
-        tail->prev = NULL;
-    }
-}
- */
